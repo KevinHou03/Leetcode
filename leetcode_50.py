3137b1f37839c64132dacf9673ed50e4daf4a0a1
kevinhou <hkaiy123@gmail.com>
2025-07-19 00:58:37 -0400

    pow

--- leetcode_50.py
+++ leetcode_50.py
@@ -9,3 +9,20 @@ def myPow(x, n):
     return x if is_neg is False else 1/x
 
 print(myPow(2,-2))
+
+def Pow(x, n):
+    if n == 0:
+        return 1
+    if n < 0:
+        return 1 / Pow(x, -n)
+    return x * Pow(x, n - 1)
+
+print(Pow(3, 4))
+
+def myPow2(x, n):
+    if n == 0:
+        return 1
+    if n < 0:
+        return 1 / myPow(x, -n)
+    half = myPow(x, n // 2)
+    return half * half if n % 2 == 0 else half * half * x
